391403bce7ade16ad31212fb696a5a15759ee81c
Tiffany Sanchez <dreaming_under_the_stars@Tiffanys-MacBook-Air.local>
2019-07-29 11:03:17 -0700

    added demo.py

---
+++ demo.py
@@ -0,0 +1,14 @@
+##Name: Tiffany S.S.
+##Date 28 July 2019
+##Description: This is a sample Python
+##file, including header and a main function
+
+
+
+
+def main():
+     name= "Jordan"
+     print("Hello, my name is", name)
+
+     if __name__ == "__main__":
+         main()
